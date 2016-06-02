d1f3cdd3d879edbe4c598105a8349a1316f3876d
Christian Panetta <chrisngabp@gmail.com>
2016-06-02 15:43:43 -0300

    first part of the initial commit

---
+++ Assets/CardboardButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CardboardButton : MonoBehaviour, IPointerClickHandler {
+
+	public Sprite enableCardboardIcon;
+	public Sprite disableCardboardIcon;
+	private Image img;
+
+	public void OnPointerClick(PointerEventData e) {
+		img = GetComponent<Image>();
+		if (GvrViewer.Instance.VRModeEnabled == true) {
+			GvrViewer.Instance.VRModeEnabled = false;
+			img.sprite = enableCardboardIcon;
+		} else {
+			GvrViewer.Instance.VRModeEnabled = true;
+			img.sprite = disableCardboardIcon;
+		}
+	}
+
+}
---
+++ Assets/getInstagramPhoto.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class getInstagramPhoto : MonoBehaviour {
+
+	IEnumerator Start() {
+		
+		/*WWW w = new WWW("https://api.instagram.com/v1/users/1058408835/media/recent/?access_token=1283749287.6f36d95.cad105b24f584a319f805973d5073a23");
+		yield return w;
+
+		string jsonStr = w.text;
+		string imgUrl = JSON.Parse (jsonStr)["data"][0]["images"]["standard_resolution"]["url"].Value; //[0]["images"]["standard_resolution"]["url"].Value
+*/
+		string imgUrl = "http://www.roadtovr.com/wp-content/uploads/2014/09/Venice.Still001.jpeg";
+		// Start a download of the given URL
+		WWW www = new WWW(imgUrl);
+
+		// Wait for download to complete
+		yield return www;
+		Renderer rend = GetComponent<Renderer>();
+		//Shader photoShader = new Shader ();
+		rend.material.mainTexture = www.texture;
+	}
+}
