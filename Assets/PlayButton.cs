468872703237573fdb7a8fbf83b7a499ecdcc96b
Christian Panetta <chrisngabp@gmail.com>
2016-10-05 15:58:18 -0300

    easymovie

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PlayButton : MonoBehaviour, IPointerClickHandler {
+
+	public void OnPointerClick(PointerEventData e) {
+		GameObject objWithVideoTexture = GameObject.Find("sphere");
+		MediaPlayerCtrl objVideoCtrl = (MediaPlayerCtrl) objWithVideoTexture.GetComponent(typeof(MediaPlayerCtrl));
+		objVideoCtrl.UnLoad();
+		objVideoCtrl.Load("prueba2.mp4");
+	}
+}
